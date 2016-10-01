a2a7b119abeaa40969b89517c2d389168507ee19
Ismail <ismail.elkorchi@gmail.com>
2016-10-01 21:50:43 +0100

    move home icons to inline svg

--- home.php
+++ home.php
@@ -22,19 +22,19 @@ function reussitepersonnelle_homepage()
 		<div class="home-features">
 			<div class="wrap">
 					<div class="one-third first">
-						<div class="icon-blog"></div>
+						<svg id="blog-icon" xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 20 20" fill="#333"><path d="M16.89 1.2l1.41 1.41c.39.39.39 1.02 0 1.41L14 8.33V18H3V3h10.67l1.8-1.8c.4-.39 1.03-.4 1.42 0zm-5.66 8.48l5.37-5.36-1.42-1.42-5.36 5.37-.71 2.12z"/></svg>
 						<h4>Blog</h4>
 						<p class="margin-bottom">Nos articles gratuits, accessible depuis le blog, vous aidons &agrave; assurer votre &eacute;panouissement personnel au quotidien.</p>
 						<p><a class="button" href="https://www.reussitepersonnelle.com/blog/">Lire le blog</a></p>
 					</div>
 					<div class="one-third">
-						<div class="icon-rss"></div>
+						<svg id="rss-icon" xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 20 20" fill="#333"><path d="M14.92 18H18C18 9.32 10.82 2.25 2 2.25v3.02c7.12 0 12.92 5.71 12.92 12.73zm-5.44 0h3.08C12.56 12.27 7.82 7.6 2 7.6v3.02c2 0 3.87.77 5.29 2.16C8.7 14.17 9.48 16.03 9.48 18zm-5.35-.02c1.17 0 2.13-.93 2.13-2.09 0-1.15-.96-2.09-2.13-2.09-1.18 0-2.13.94-2.13 2.09 0 1.16.95 2.09 2.13 2.09z"/></svg>
 						<h4>Newsletter</h4>
 						<p class="margin-bottom">Recevez nos conseils exclusifs dir&eacute;ctement sur votre boite email. Votre vie ne sera plus jamais la m&ecirc;me.</p>
 						<p><a class="button" href="https://www.reussitepersonnelle.com/newsletter/">Inscrivez-vous</a></p>
 					</div>
 					<div class="one-third last">
-						<div class="icon-desktop"></div>
+						<svg id="desktop-icon" xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 20 20" fill="#333"><path d="M3 2h14c.55 0 1 .45 1 1v10c0 .55-.45 1-1 1h-5v2h2c.55 0 1 .45 1 1v1H5v-1c0-.55.45-1 1-1h2v-2H3c-.55 0-1-.45-1-1V3c0-.55.45-1 1-1zm13 9V4H4v7h12zM5 5h9L5 9V5z"/></svg>
 						<h4>&Agrave; propos</h4>
 						<p class="margin-bottom">Notre site vous donne les outils pour vous aider &agrave; d&eacute;velopper une existence sereine et heureuse.</p>
 						<p><a class="button" href="https://www.reussitepersonnelle.com/a-propos/">En savoir plus</a></p>
