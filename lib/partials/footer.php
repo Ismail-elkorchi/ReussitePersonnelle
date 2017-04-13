f80519d5e3278568ffb8c9cf304f381082f014f7
Ismail el korchi <ismail.elkorchi@gmail.com>
2017-04-13 19:47:55 +0100

    Align footer divs using buit-in Wordpress classes

--- lib/partials/footer.php
+++ lib/partials/footer.php
@@ -1,11 +1,11 @@
 <div class="site-footer">
 	<div class="wrap">
-		<div class="left">
+		<div class="alignleft">
 			<p>
 				R&eacute;ussite Personnelle &copy; 2012-<?php echo date('Y'); ?>  &middot; <a href="https://www.reussitepersonnelle.com/politique-confidentialite/">Politique de confidentialit&eacute;</a> | <a href="https://www.reussitepersonnelle.com/politique-de-remboursement/">Politique de remboursement</a>
 			</p>
 		</div>
-		<div class="right">
+		<div class="alignright">
 			<p>Langues &middot; <a href="https://www.reussitepersonnelle.com/ar/">Arabe</a> | Fran&ccedil;ais</p>
 		</div>
 	</div>
